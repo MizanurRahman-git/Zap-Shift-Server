f98849a8f1df22d2c70b7dd4391cb0ee587335b0
MizanurRahmanHridoy <mizanurrahmanhridoy42@gmail.com>
2025-11-23 02:07:23 +0600

    add payment chackout related api

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const app = express();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_SECRET);
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const port = process.env.PORT || 3000;
 
@@ -27,26 +29,65 @@ app.get("/", (req, res) => {
 async function run() {
   try {
     await client.connect();
-    const db = client.db('zap_shift_db')
-    const parcelCollection = db.collection('parcels')
+    const db = client.db("zap_shift_db");
+    const parcelCollection = db.collection("parcels");
 
+    app.get("/parcels", async (req, res) => {
+      const query = {};
+      const { email } = req.query;
+      if (email) {
+        query.senderemail = email;
+      }
+      const options = { sort: { createdAt: -1 } };
+      const cursor = parcelCollection.find(query, options);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
-    app.get('/parcels', async(req, res)=> {
-        const query = {}
-        const {email} = req.query
-        if(email){
-            query.senderemail = email
-        }
-        const cursor = parcelCollection.find(query)
-        const result = await cursor.toArray()
-        res.send(result)
-    })
+    app.get("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelCollection.findOne(query);
+      res.send(result);
+    });
 
-    app.post('/parcels', async (req, res)=> {
-        const parcel = req.body
-        const result = await parcelCollection.insertOne(parcel)
-        res.send(result)
-    })
+    app.post("/parcels", async (req, res) => {
+      const parcel = req.body;
+      parcel.createdAt = new Date();
+      const result = await parcelCollection.insertOne(parcel);
+      res.send(result);
+    });
+
+    app.delete("/parcels/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await parcelCollection.deleteOne(query);
+      res.send(result);
+    });
+
+    // payment chackout related api
+
+    app.post("/create-checkout-session", async (req, res) => {
+      const paymentInfo = req.body;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            // Provide the exact Price ID (for example, price_1234) of the product you want to sell
+            price_data: {
+              currency: "USD",
+              product_data:{
+                name: paymentInfo.parcelName
+              },
+              unit_amount:1500,
+            },
+            quantity: 1,
+          },
+        ],
+        customer_email: paymentInfo.senderemail,
+        mode: "payment",
+        success_url: `${process.env.SITE_DOMAIN}/dashboard/payment-success`,
+      });
+    });
 
     await client.db("admin").command({ ping: 1 });
     console.log(
